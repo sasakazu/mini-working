36e7781ac7386eaff6d6406abd3a92ae1213eb76
sasakazu <sasakura.company@gmail.com>
2018-07-05 19:42:15 +0900

    pod install.....

---
+++ Pods/Target Support Files/Pods-mini-workingTests/Pods-mini-workingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mini_workingTests : NSObject
+@end
+@implementation PodsDummy_Pods_mini_workingTests
+@end
---
+++ Pods/Target Support Files/Pods-mini-workingUITests/Pods-mini-workingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mini_workingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_mini_workingUITests
+@end
