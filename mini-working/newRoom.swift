5a33028ebb3ccb118658986c720cfbae356b1d67
sasakazu <sasakura.company@gmail.com>
2018-07-05 19:37:05 +0900

    account ui changed....

---
+++ mini-working/newRoom.swift
@@ -0,0 +1,27 @@
+//
+//  newRoom.swift
+//  mini-working
+//
+//  Created by 笹倉 一也 on 2018/07/05.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class newRoom: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+
+}
